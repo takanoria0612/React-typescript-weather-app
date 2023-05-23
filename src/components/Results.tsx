51ddba756c774adae35aa62e528c9a1dd3e48474
takanoria0612 <takanorihiraki06@gmail.com>
2023-05-23 17:53:19 +0900

    5/23 morning

---
+++ src/components/Results.tsx
@@ -0,0 +1,26 @@
+type ResultsPropsType = {
+    results: {
+        country: string;
+        cityName: string;
+        temperature: string;
+        conditionText: string;
+        icon: string;
+    }
+}
+const Results = (props:ResultsPropsType) => {
+    return (
+    <div>
+        {props.results.country && <div>{props.results.country}</div>}
+        {props.results.cityName && <div>{props.results.cityName}</div>}
+        {props.results.temperature && <div>{props.results.temperature} <span>°c</span></div>}
+        {props.results.conditionText &&
+            <div>
+                <img src={props.results.icon} alt="icon"/>
+                <span>{props.results.conditionText}</span>
+            </div>
+        }
+    </div>
+    );
+};
+
+export default Results;
