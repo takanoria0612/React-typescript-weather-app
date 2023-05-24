a76795f6a54b953691da504c6440618d7ec15176
takanoria0612 <takanorihiraki06@gmail.com>
2023-05-24 18:41:40 +0900

    5/23デプロイ終了後、ReadMeファイル更新5

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -3,30 +3,32 @@ type ResultsPropsType = {
         country: string;
         cityName: string;
         temperature: string;
-        conditionText: string;
+        conditionText: string; 
         icon: string;
     }
 }
-const Results = (props:ResultsPropsType) => {
+
+const Results = ({ results }: ResultsPropsType) => {
+    const { country, cityName, temperature, conditionText, icon } = results;
     return (
-        <div>
-            {props.results.country && 
-                <div className="results-country">{props.results.country}</div>
+        <>
+            {country && 
+                <div className="results-country">{country}</div>
             }
-            {props.results.cityName && 
-                <div className="results-city">{props.results.cityName}</div>
+            {cityName && 
+                <div className="results-city">{cityName}</div>
             }
-            {props.results.temperature && 
-                <div className="results-temp">{props.results.temperature} <span>°C</span></div>
+            {temperature && 
+                <div className="results-temp">{temperature} <span>°C</span></div>
             }
-            {props.results.conditionText && 
+            {conditionText && 
                 <div className="results-condition">
-                    <img src={props.results.icon} alt="icon"/>
-                    <span>{props.results.conditionText}</span>
+                    <img src={icon} alt="icon"/>
+                    <span>{conditionText}</span>
                 </div>
             }
-    </div>
+        </>
     );
 };
 
-export default Results;
+export default Results;;
